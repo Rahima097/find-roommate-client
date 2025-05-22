67b2549bd282cdfda16513c7795561beabe0a985
Rahima Khatun <mrahimakhatun281@gmail.com>
2025-05-23 00:49:34 +0600

    extra section faq added

---
+++ src/Components/Faq.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import faqImg from './../assets/faq.png'
+
+const Faq = () => {
+  return (
+    <section className="max-w-7xl mx-auto px-4 py-16 flex flex-col lg:flex-row items-center gap-12">
+      <div className="flex-1">
+        <img
+          src={faqImg }
+          alt="Roommate Discussion"
+          className="w-full rounded-2xl "
+        />
+      </div>
+
+      <div className="flex-1 space-y-6">
+        <h2 className="text-4xl font-bold text-secondary">Frequently <span className='text-primary'>Asked Questions</span></h2>
+        <div className="join join-vertical  w-full">
+          <div className="collapse collapse-arrow join-item border border-base-300 my-2 bg-base-100">
+            <input type="radio" name="faq-accordion" defaultChecked />
+            <div className="collapse-title text-lg font-medium text-secondary">
+              How do I find a suitable roommate?
+            </div>
+            <div className="collapse-content">
+              <p>Use our smart listings and search (location, lifestyle, budget) and read verified profiles. You can message potential roommates directly on our platform.</p>
+            </div>
+          </div>
+
+          <div className="collapse collapse-arrow join-item border border-base-300 my-2 bg-base-100">
+            <input type="radio" name="faq-accordion" />
+            <div className="collapse-title text-lg font-medium text-secondary">
+              Is it free to post a roommate listing?
+            </div>
+            <div className="collapse-content">
+              <p>Yes! You can post your listing for free. Premium options are available for more visibility.</p>
+            </div>
+          </div>
+
+          <div className="collapse collapse-arrow join-item border border-base-300 my-2 bg-base-100">
+            <input type="radio" name="faq-accordion" />
+            <div className="collapse-title text-lg font-medium text-secondary">
+              Are the users verified?
+            </div>
+            <div className="collapse-content">
+              <p>Yes, all users go through email verification and have the option to add more ID details for added trust.</p>
+            </div>
+          </div>
+
+          <div className="collapse collapse-arrow join-item border border-base-300 my-2 bg-base-100">
+            <input type="radio" name="faq-accordion" />
+            <div className="collapse-title text-lg font-medium text-secondary">
+              Can I update or delete my listing?
+            </div>
+            <div className="collapse-content">
+              <p>Absolutely. You can edit or remove your listing anytime from your dashboard.</p>
+            </div>
+          </div>
+
+          <div className="collapse collapse-arrow join-item border border-base-300 my-2 bg-base-100">
+            <input type="radio" name="faq-accordion" />
+            <div className="collapse-title text-lg font-medium text-secondary">
+              How do I stay safe when meeting roommates?
+            </div>
+            <div className="collapse-content">
+              <p>Meet in public places first, share minimal personal info, and use our in-platform messaging until you feel comfortable.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Faq;
--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -3,6 +3,7 @@ import Banner from '../Components/Banner';
 import { useLoaderData } from 'react-router';
 import FeatureRoommate from '../Components/FeatureRoommate';
 import About from '../Components/About';
+import Faq from '../Components/Faq';
 
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
             <Banner></Banner>
             <FeatureRoommate roommates={roommates} />
             <About></About>
+            <Faq></Faq>
             
         </div>
     );
